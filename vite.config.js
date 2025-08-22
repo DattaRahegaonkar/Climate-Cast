4ae1a75c3bcddce2e6c3ab0c27586b1a7835c84c
Datta Rahegaonkar <rahegaonkardatta@gmail.com>
2025-08-22 12:33:33 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 4173,
+    strictPort: true,
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    hmr: {
+      clientPort: 443,
+      protocol: 'wss',
+    },
+  },
 })
